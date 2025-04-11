c4a8ed3855f39a92062026431831d3255145475a
Jan Zálešák <zalesak@vukoz.cz>
2025-04-11 07:49:31 +0200

    DATA ACCESS CONDITIONS text updates

--- pages/6_terms_and_conditions.py
+++ pages/6_terms_and_conditions.py
@@ -10,65 +10,45 @@ st.write(
     """
 **A. WILDCARD DENDROMETRIC DATABASE ACCESS**
 
-1. Here we specify the conditions under which the Data Curator shall provide the on-line ‘read only’ access to the content of the WILDCARD Relational PostgreSQL dendrometric database to the Data User
-
+Here we specify the conditions under which the Data Curator shall provide the on-line ‘read only’ access to the content of the WILDCARD Relational PostgreSQL dendrometric database to the Data User
 
 And/Or
 
-
-2. The Data Curator shall carry-out data queries and provide specific data assemblies on the demand of the Data User
-)
+The Data Curator shall carry-out data queries and provide specific data assemblies on the demand of the Data User)
 
 **B. TERMS and CONDITIONS of DATA USE**
- 
 
-1. The data User has rights to use the data only within the framework of the WILDCARD HE project exclusively for purposes specified in the EuFoRIa WILDCARD Project Proposal Parts A and B.
+The data User has rights to use the data only within the framework of the WILDCARD project exclusively for purposes specified in the EuFoRIa WILDCARD Project Proposal Parts A and B.
 
+In particular, commercial use of the data is not permitted.
 
-2. In particular, commercial use of the data is not permitted.
-
-
-3. Any sharing of the data or its directly derived characteristics to third parties (outside the WILDCARD project) is not permitted, with the exception of cases directly linked to publications in scientific journals (see point C4)
+Any sharing of the data or its directly derived characteristics to third parties (outside the WILDCARD project) is not permitted, with the exception of cases directly linked to publications in scientific journals (see point C4)
 
 Likewise, no rights of access, use or exploitation may be granted or transferred to third parties.
 
 Access to the data is to be regulated by the Data User in such a way that unauthorised data access is excluded.
 
+The right of use is limited in time and ends on 31. 12. 2027.
 
-4. The right of use is limited in time and ends on 31. 12. 2027.
+On this date, the Data Curator must terminate the data access to the Data User. The Data User must delete all provided dendrometric data assemblies stored outside of the central WILDCARD database, with the exception of the data necessary for documentation and archiving purposes and finishing publications directly connected to the WILDCARD project.
 
-On this date, the data curator must terminate the data access to the Data User. The Data User must delete all provided dendrometric data assemblies stored outside of the central WILDCARD database, with the exception of the data necessary for documentation and archiving purposes and finishing publications directly connected to the WILDCARD project.
+All rights to the data shall remain with the original data providers.
 
-
-5. All rights to the data shall remain with the original data providers.
-
-
-6. In the event that the data is used contrary to the terms of these conditions, the right to access and use the data is withdrawn with immediate effect.
-
-
+In the event that the data is used contrary to the terms of these conditions, the right to access and use the data is withdrawn with immediate effect.
 
 **C. PUBLICATIONS**
 
+If the use of data results in publication, the original data provider is involved in the publication process and updated on a regular basis. It is up to the main author of a publication to decide if the provided data can be integrated in the planned scientific study.
 
-1. If the use of data results in publication, the original data provider is involved in the publication process and updated on a regular basis. It is up to the main author of a publication to decide if the provided data can be integrated in the planned scientific study.
+Data providers are offered co-authorship provided that they also contribute to the process of manuscript writing and/or revision and editing before submission.
 
+The acknowledgments of the resulting publications will include a statement referring to the institution of the original data provider.
 
-2. Data providers are offered co-authorship provided that they also contribute to the process of manuscript writing and/or revision and editing before submission.
-
-
-3. The acknowledgments of the resulting publications will include a statement referring to the institution of the original data provider.
-
-
-4. Before data is delivered for archiving required by a journal in which analyses of the data are published, the nature and form of the data delivered must be agreed upon.
-
-
-
+Before data is delivered for archiving (required by a journal in which analyses of the data are published), the nature and form of the data delivered must be agreed upon.
 
 **D. LIABILITY**
 
-
-1. The data are based on the current state of scientific knowledge. However, there is no liability on the completeness, usefulness or accuracy of the data.  
-
+The data are based on the current state of scientific knowledge. However, there is no liability on the completeness, usefulness or accuracy of the data.
  
 """)
 
